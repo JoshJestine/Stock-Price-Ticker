b02e1d73b0eb820303231e100f12a78c21e05ffc
Josh Jestine <joshjustin007@gmail.com>
2023-04-22 00:06:27 +0530

    Added Indian Stocks

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ import yfinance as yf
 import streamlit as st
 
 st.write("""
-# Simple Stock Price App
-
-Shown are the stock closing price and volume of Google!
+# Stock Ticker Web App for Indian Stocks which have returned more than 20% for the past 5 years
 
 """)
 
+st.write(""" # Pidilite Industries Stock Ticker """)
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'PIDILITIND.NS'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='5Y', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
@@ -22,13 +21,27 @@ st.line_chart(tickerDf.Volume)
 
 st.write("""====================""")
 
-st.write(""" # TCS Stock Price App """)
+st.write(""" # Bajaj Finance Stock Ticker """)
 
-tickerSymbol = 'TCS.NS'
+tickerSymbol = 'BAJFINANCE.NS'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='5y', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
+st.write("""====================""")
+
+st.write(""" # GMM Pfaudler Stock Ticker """)
+
+tickerSymbol = 'BAJFINANCE.NS'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='5y', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
